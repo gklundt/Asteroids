6e9e0076a6c1a4674adfe97a288b249b3fc93b29
gklundt <gklundt@gmail.com>
2015-02-22 23:23:44 -0600

    Removing comment

--- src/Asteroids.java
+++ src/Asteroids.java
@@ -2,7 +2,7 @@
 public class Asteroids {
 
     public synchronized static void main(String[] args) {
-/* This is a big fat comment*/
+
         AbstractGameWindow gameWindow = GameWindowFactory.GetGameWindow(GameWindowFactory.ASTEROIDS);
         gameWindow.setVisible(true);
         gameWindow.resetGame();
